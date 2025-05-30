11a22289fdfb3beb371f8dad1c992c38b4ca561a
Sudenaz Yonezer <sudenazyonezer@posta.mu.edu.tr>
2025-05-30 12:03:28 +0300

    week-12

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        StackLinkedList<Object> stack =new StackLinkedList<>();
+        stack.push(3);
+        stack.push(45);
+        stack.push(3.4);
+        stack.push("This is a test");
+        stack.displayStack();
+
+
+
+        /*try{
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
+        ArrayList<String> stringList= new ArrayList();
+        stringList.add("String");
+        stringList.add("Ali");
+        System.out.println(stringList);
+
+        HashMap<String,Double> grades = new HashMap();
+        grades.put("Ali",10.0);
+        grades.put("Pelin",75.4);
+        grades.put("Koray",4.5);
+        System.out.println(grades);
+        grades.put("Ali",100.0);
+        System.out.println(grades);
+        */
+
+
+
+
+    }
+}
